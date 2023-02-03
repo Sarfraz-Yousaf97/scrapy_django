a1d089abc0594a681e2d7d68f91d781f32d118a3
Sarfraz-Yousaf97 <muhammadsarfrazyousaf009@gmail.com>
2023-02-03 15:58:44 +0500

    oatent added

--- scrapy_app/scrapy_app/spiders/newvalue.py
+++ scrapy_app/scrapy_app/spiders/newvalue.py
@@ -7,7 +7,7 @@ class FreePatentSpider(scrapy.Spider):
     allowed_domains = ['freepatentsonline.com']
     search_value = 'iphone'
     start_urls = [f'https://www.freepatentsonline.com/result.html?sort=relevance&srch=top&query_txt={search_value}&submit=&patents_us=on']
-    
+     
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
 
     def parse(self, response):
